bd31c9c82a837d3926fbe852ad3b26f8aa2f4470
defseg <6988161+defseg@users.noreply.github.com>
2015-04-02 00:59:34 -0400

    Add route for app page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
 
   def splash
   end
-  
+
+  def app
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   put '/reload-all' => 'feeds#reload_all', as: :reload_all_feeds
   put '/feeds/:id/reload' => 'feeds#reload', as: :reload_feed
   get '/splash' => 'home#splash', as: :splash
+  get '/app' => 'home#app', as: :app
   resources :feeds
 
   namespace :api do
